1c50217ec9842383ddf7ba820e31632e9d7ba9fd
Bryce Graffin <brycegraffin2019@yahoo.com>
2025-11-09 00:19:38 -0600

    cleaned up imports

--- mars_orbiter.py
+++ mars_orbiter.py
@@ -5,8 +5,6 @@ Author: Bryce Graffin
 Date: 08 November 2025
 """
 import os  # for updating where the window will be located if the user decides to exit fullscreen mode
-import math  # for handling math calculations
-import random  # for handling starting locations of the satellite sprite
 import pygame as pg  # for running the game
 
 # region === IMPORT MODULES FROM SRC ===
--- src/satellite.py
+++ src/satellite.py
@@ -4,8 +4,6 @@ This module contains the Satellite class and methods.
 Author: Bryce Graffin
 Date: 08 November 2025
 """
-import os  # for updating where the window will be located if the user decides to exit fullscreen mode
-import math  # for handling math calculations
 import random  # for handling starting locations of the satellite sprite
 import pygame as pg  # for running the game
 
