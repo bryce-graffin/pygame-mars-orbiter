538824c5fb0e90ab3119d8dab14a74aeed5dc3c7
Bryce Graffin <brycegraffin2019@yahoo.com>
2025-11-08 23:07:49 -0600

    added color table

---
+++ mars_orbiter.py
@@ -0,0 +1,10 @@
+"""
+This module is the main executable of the Mars Orbiter game.
+
+Author: Bryce Graffin
+Date: 08 November 2025
+"""
+import os  # for updating where the window will be located if the user decides to exit fullscreen mode
+import math  # for handling math calculations
+import random  # for handling starting locations of the satellite sprite
+import pygame as pg  # for running the game
---
+++ src/utils/color_table.py
@@ -0,0 +1,13 @@
+"""
+This module stores the colors used in the game that can be referenced by other modules within the project.
+"""
+
+# basic default color table
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# extra colors
+LT_BLUE = (173,  216, 230)
