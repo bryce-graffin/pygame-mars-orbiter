a561598ec626ecb6b34e591fb1bea59c5d963a1e
Bryce Graffin <brycegraffin2019@yahoo.com>
2025-11-08 23:43:44 -0600

    Update mars_orbiter.py

--- mars_orbiter.py
+++ mars_orbiter.py
@@ -8,3 +8,14 @@ import os  # for updating where the window will be located if the user decides t
 import math  # for handling math calculations
 import random  # for handling starting locations of the satellite sprite
 import pygame as pg  # for running the game
+
+
+def main():
+    """
+    Set up labels and instructions, create objects, & run the game loop.
+    """
+    pg.init()  # initializes pygame
+
+    # set up the display
+    os.environ['SDL_VIDEO_WINDOWS_POS'] = '700, 100'  # set origin of the game window
+    screen = pg.display.set_mode((800, 645), pg.FULLSCREEN)  # set to full screen
