39bec4a397148182e2660bfa3fff713fb29518ba
Bryce Graffin <brycegraffin2019@yahoo.com>
2025-11-09 21:29:56 -0600

    add check_keys(), thruster(), locate(), rotate(), path(), and update()
    adds code to handle updating the Satellite objects movement, location, heading, direction, and fuel attributes during the game loop

---
+++ src/planet.py
@@ -0,0 +1,8 @@
+"""
+This module contains the `Planet` class and methods.
+
+Author: Bryce Graffin
+Date: 09 November 2025
+"""
+class Planet(pg.sprite.Sprite):
+    pass
--- src/satellite.py
+++ src/satellite.py
@@ -1,14 +1,16 @@
 """
-This module contains the Satellite class and methods.
+This module contains the `Satellite` class and methods.
 
 Author: Bryce Graffin
 Date: 08 November 2025
 """
 import random  # for handling starting locations of the satellite sprite
+import math  # for calculating distance and heading
 import pygame as pg  # for running the game
 
 # import utils
 from src.utils.color_table import colors
+from src.planet import Planet
 
 class Satellite(pg.sprite.Sprite):
     """
@@ -43,6 +45,7 @@ class Satellite(pg.sprite.Sprite):
         # set heading (dish orientation), fuel, mass, and distance to the planet
         self.heading = 0
         self.fuel = 100
+        self.fuel_adjustment_factor = 2  # how much fuel is consumed per thruster firing, great for varying difficulty
         self.mass = 1
         self.distance = 0
 
@@ -51,3 +54,107 @@ class Satellite(pg.sprite.Sprite):
         self.thrust.set_volume(0.07)  # valid values are 0-1
 
         # endregion
+
+    # region === MOVEMENT HANDLING METHODS ===
+
+    def check_keys(self):
+        """
+        Listens for user to input key presses and calls the `thruster()` method.
+        """
+        keys = pg.key.get_pressed()  # get the current state of all keyboard buttons
+
+        if keys[pg.K_LEFT]:  # decrease Dx
+            self.thruster('LEFT')
+        if keys[pg.K_RIGHT]:  # increase Dx
+            self.thruster('RIGHT')
+        if keys[pg.K_UP]:  # decrease Dy
+            self.thruster('UP')
+        if keys[pg.K_DOWN]:  # increase Dy
+            self.thruster('DOWN')
+
+    def thruster(self, direction: str):
+        """
+        Updates the satellites velocity components. Receives the user input and adds/subtracts from dx, dy, and fuel
+        accordingly.
+
+        :param direction: A string representing which thruster direction was fired by the user.
+        :type direction: str
+        """
+        if self.fuel > 0:  # only allow thruster firing if there is fuel
+            # x-coordinate system is standard, so increasing x moves right on the screen
+            if direction == 'LEFT':
+                self.dx -= 0.05
+            elif direction == 'RIGHT':
+                self.dx += 0.05
+            # y-coordinate system is inverted in pygame, so decreasing y moves up the screen
+            elif direction == 'UP':
+                self.dy -= 0.05
+            elif direction == 'DOWN':
+                self.dy += 0.05
+
+            self.fuel -= self.fuel_adjustment_factor  # decrease fuel by 1 unit per thruster firing
+            self.thrust.play()  # play the thruster sound effect
+
+    def locate(self, planet: Planet):
+        """
+        Calculates the distance of the `Satellite` object from the `Planet` object and determines heading for pointing
+        the radar dish towards the planet at all times.
+
+        :param planet: Planet object that the satellite is orbiting.
+        :type planet: Planet
+        """
+        px, py = planet.rect.center  # get the center coords of the planet
+        dist_x = self.x - px  # horizontal distance from planet center
+        dist_y = self.y - py  # vertical distance from planet center
+
+        # get direction to planet to find heading and distance
+        direction_to_planet_radians = math.atan2(dist_x, dist_y)  # convert to radians because pygame uses degrees
+        self.heading = direction_to_planet_radians * (180 / math.pi)  # convert to degrees for heading
+        self.heading -= 90  # adjust because the sprite is flipped (flying tail-first)
+        self.distance = math.hypot(dist_x, dist_y)  # calculate Euclidian, straight-line distance to planet center
+       
+    def rotate(self):
+        """
+        Rotates `Satellite` (using degrees for `pygame`) so the radar dish faces the planet's center.
+
+        ### Potential Future Improvement:
+        - Consider factoring in a system (in harder game-modes) where the player needs to purchase real-world elements
+        like a flywheel or reaction wheels to automatically orient the dish towards the planet - otherwise the satellite
+        will face in the same direction and the player will need to manually rotate it or wait for the planet to rotate
+        as it orbits, decreasing time and therefore score.
+        """
+        self.image = pg.transform.rotate(self.image_sat, self.heading)  # rotate the satellite image
+        self.rect = self.image.get_rect()
+
+    def path(self):
+        """
+        Draws the path of the `Satellite` as it orbits and updates after receiving input that updates it's position and
+        movement.
+        """
+        # update position and draw line to trace the satellite's orbital path
+        last_center = (self.x, self.y)  # store last-known position before updating
+        self.x += self.dx  # update x coord
+        self.y += self.dy  # update y coord
+        pg.draw.line(
+            surface=self.background,
+            color=colors['WHITE'],
+            start_pos=last_center,
+            end_pos=(self.x, self.y),
+            width=2  # TODO: update this after testing for different line thicknesses
+        )
+
+    # endregion
+
+    def update(self):
+        """
+        Updates the `Satellite` object in the game loop.
+        """
+        self.check_keys()  # check for user input
+        self.rotate()  # rotate the satellite to face the planet
+        self.path()  # draw path
+        self.rect.center = (self.x, self.y)  # update the rectangle position
+
+        # change image to crashed (red) if entering the Planet's atmoshphere
+        if self.dx == 0 and self.dy == 0:
+            self.image = self.image_crash
+            self.image.set_colorkey(colors['BLACK'])
